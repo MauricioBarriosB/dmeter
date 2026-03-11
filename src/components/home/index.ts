fc215704e68c7ededff86273fbcecd3a151aae61
MauricioBarriosB <55992895+MauricioBarriosB@users.noreply.github.com>
2026-03-11 17:24:22 -0300

    Delete src directory

--- src/components/home/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MeterFeatures } from "./MeterFeatures";
-export { default as AcousticsFeatures } from "./AcousticsFeatures";
-export { default as MaterialsFeatures } from "./MaterialsFeatures";
-export { default as InstrumentsFeatures } from "./InstrumentsFeatures";
