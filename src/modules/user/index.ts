8616bfa4a7735d3e391ae832247efe06225d90a6
MauricioBarriosB <55992895+MauricioBarriosB@users.noreply.github.com>
2026-04-06 10:07:04 -0400

    Delete src directory

--- src/modules/user/index.ts
+++
@@ -1,5 +0,0 @@
-// User module exports
-export { AuthProvider, useAuthContext } from "./context/AuthContext";
-export { useAuth } from "./hooks/useAuth";
-export { default as ProtectedRoute } from "./components/ProtectedRoute";
-export type * from "./types";
